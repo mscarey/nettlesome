05d2e567f882a2c5aedc9dacb9ffea513903fd22
Matt Carey <matt@authorityspoke.com>
2021-02-20 17:11:45 -0600

    add means and implies tests for Doctrine

--- tests/test_doctrines.py
+++ tests/test_doctrines.py
@@ -8,26 +8,56 @@ class TestDoctrine:
         "$concept was one honking great idea",
         terms=Entity("namespaces", plural=True),
     )
-    with_authority = Doctrine(statement=namespaces, authority=Entity("Tim Peters"))
+    generic_authority = Doctrine(statement=namespaces, authority=Entity("Twitter user"))
+    specific_authority = Doctrine(
+        statement=namespaces, authority=Entity("Tim Peters", generic=False)
+    )
     no_authority = Doctrine(statement=namespaces, authority=None)
 
     def test_dictum_string(self):
-        assert "<namespaces> were one honking great idea" in str(self.with_authority)
+        assert "<namespaces> were one honking great idea" in str(self.generic_authority)
 
     def test_means_self(self):
-        assert self.with_authority.means(self.with_authority)
+        assert self.generic_authority.means(self.generic_authority)
+
+    def test_means_self_with_no_authority(self):
+        assert self.no_authority.means(self.no_authority)
+
+    def test_means_self_with_specific_authority(self):
+        assert self.specific_authority.means(self.specific_authority)
 
     def test_not_same_meaning(self):
-        assert not self.with_authority.means(self.no_authority)
+        assert not self.generic_authority.means(self.no_authority)
 
     def test_not_same_meaning_None_on_left(self):
-        assert not self.no_authority.means(self.with_authority)
+        assert not self.no_authority.means(self.generic_authority)
 
-    def test_implication(self):
-        assert self.with_authority.implies(self.no_authority)
+    def test_not_same_meaning_specific_entity(self):
+        assert not self.specific_authority.means(self.no_authority)
+
+    def test_not_same_meaning_None_on_left_specific_entity(self):
+        assert not self.no_authority.means(self.specific_authority)
+
+    def test_no_authority_implies_none(self):
+        assert self.no_authority.implies(self.no_authority)
+
+    def test_generic_authority_implies_none(self):
+        assert self.generic_authority.implies(self.no_authority)
+
+    def test_no_implication_of_specific(self):
+        assert not self.generic_authority.implies(self.specific_authority)
+
+    def test_implication_by_specific(self):
+        assert self.specific_authority.implies(self.generic_authority)
+
+    def test_no_implication_of_specific_by_none(self):
+        assert not self.no_authority.implies(self.specific_authority)
+
+    def test_implication_of_none_by_specific(self):
+        assert self.specific_authority.implies(self.no_authority)
 
     def test_implies_self(self):
-        assert self.with_authority.implies(self.with_authority)
+        assert self.generic_authority.implies(self.generic_authority)
 
     def test_no_implication_no_authority(self):
-        assert not self.no_authority.implies(self.with_authority)
+        assert not self.no_authority.implies(self.generic_authority)
