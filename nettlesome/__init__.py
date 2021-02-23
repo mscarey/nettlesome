d7a591e7ae1738458973eff5d5d238f93efb467f
Matt Carey <matt@authorityspoke.com>
2021-02-22 21:58:17 -0600

    set version to 0.1.0

---
+++ nettlesome/__init__.py
@@ -0,0 +1,9 @@
+"""Nettlesome: Simplified semantic reasoning."""
+
+from .doctrines import Doctrine
+from .entities import Entity
+from .groups import FactorGroup
+from .predicates import Comparison, Predicate
+from .statements import Statement
+
+__version__ = "0.1.0"
