4d42ce8ac8b070444bc44dd35706cd45d3f73671
Matt Carey <matt@authorityspoke.com>
2023-12-03 20:22:32 -0600

    replace deprecated "schema" attribute

--- tests/test_spec.py
+++ tests/test_spec.py
@@ -3,7 +3,7 @@ from nettlesome.statements import Assertion
 
 class TestSpec:
     def test_make_json_spec(self):
-        schema = Assertion.schema()
+        schema = Assertion.model_json_schema()
         ref = schema["$defs"]["Assertion"]["properties"]["statement"]["$ref"]
         ent_ref = schema["$defs"]["Assertion"]["properties"]["authority"]["anyOf"][0][
             "$ref"
